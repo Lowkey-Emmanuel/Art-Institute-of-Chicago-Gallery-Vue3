7fad51a6461484aa9750b32ae3cde7bd81d485d9
unknown <KSERV@KSV64.kservgroup2.sn>
2023-11-20 17:00:57 +0000

    Feat-components and pages created - routes setup - gallery search and grid unfinished - login/signup/404 pages to be integrated

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,101 @@
+<script setup lang="ts">
+import footerLogo from '@/assets/img/logo_black.jpg'
+</script>
+<template>
+  <footer class="footer">
+    <div class="footer-left">
+      <div class="footer__img">
+        <img :src="footerLogo" alt="Footer logo" />
+      </div>
+      <p class="footer__text">ART INSTITVTE CHICAGO</p>
+    </div>
+    <div class="footer-right">
+      <ul class="footer__items">
+        <li>
+          <a href="https://www.youtube.com/user/ArtInstituteChicago"><i class="fa-brands fa-youtube"></i></a>
+        </li>
+        <li>
+          <a href="https://twitter.com/artinstitutechi"><i class="fa-brands fa-twitter"></i></a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/artinstitutechi/"
+            ><i class="fa-brands fa-instagram"></i
+          ></a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/artic"><i class="fa-brands fa-facebook"></i></a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss">
+@import '@/assets/scss/_base.scss';
+
+.footer {
+  width: 100%;
+  height: 66px;
+  padding-inline: 8rem;
+  background-color: var(--clr-brand-900);
+  color: var(--clr-brand-100);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+
+  &-left {
+    width: 100%;
+    display: flex;
+    justify-content: stretch;
+    align-items: center;
+    gap: 0.8em;
+  }
+
+  &__img {
+    width: 30px;
+    aspect-ratio: 1/1;
+
+    & > img {
+      color: var(--clr-brand-100);
+      max-width: 100%;
+      display: block;
+      border: var(--clr-brand-100) 3px solid;
+    }
+  }
+
+  &__text {
+    font-size: 12px;
+    line-height: 24px;
+  }
+
+  &__items {
+    display: flex;
+    justify-content: center;
+    justify-items: center;
+    gap: 1rem;
+  }
+
+  & li > a {
+    display: block;
+    color: inherit;
+    text-decoration: none;
+  }
+
+  .fa-brands {
+    display: block;
+    font-size: 12px;
+    border: var(--clr-brand-100) 1px solid;
+    padding: 0.3em;
+    border-radius: 8px;
+    &:hover {
+      border: var(--clr-brand-200) 1px solid;
+      color: var(--clr-brand-200);
+      transform: scale(1.5);
+    }
+  }
+}
+</style>
---
+++ src/views/RechercheView.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="gallery">
+    <div class="gallery__nav">
+      <div class="gallery__form">
+        <form class="gallery__form--input">
+          <input type="text" placeholder="Rechercher..." />
+        </form>
+      </div>
+      <div class="gallery__form--pagination">paginating...</div>
+    </div>
+    <section class="gallery__grid"></section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_base.scss';
+
+.gallery {
+  flex: 1;
+  margin-inline: 5em;
+  margin-top: 2rem;
+  &__nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__form--input > input {
+    width: 335px;
+    height: 45px;
+    background-color: var(--clr-brand-100);
+    padding-inline: 1em;
+
+    border: var(--clr-brand-200) 1px solid;
+    border-radius: 10px;
+  }
+}
+</style>
